bc522f31de38421b9f936e4f7e0c50f95ff61625
JieXiao <jiexiao111@gmail.com>
2017-12-30 00:52:08 +0800

    Site updated: 2017-12-30 00:52:08

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sun Dec 03 2017 22:42:34 GMT+0800 (CST)
+// build time:Sat Dec 30 2017 00:52:02 GMT+0800 (CST)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
